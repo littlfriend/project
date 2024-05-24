67c76a18671cddbababd320164621513001a4212
littlfriend <kaung_linn_thant@s2024.ssts.edu.sg>
2024-05-24 21:38:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggle() {
+    var toggler = document.getElementById("button1");
+    if (toggler.style.display === "none") {
+      toggler.style.display = "block";
+    } else {
+      toggler.style.display = "none";
+    }
+  }
+function toggle2() {
+    var toggler = document.getElementById("button2");
+    if (toggler.style.display === "none") {
+      toggler.style.display = "block";
+    } else {
+      toggler.style.display = "none";
+    }
+  }
+  function toggle3() {
+    var toggler = document.getElementById("button3");
+    if (toggler.style.display === "none") {
+      toggler.style.display = "block";
+    } else {
+      toggler.style.display = "none";
+    }
+  }
